420fc0b724b6de1c45c0215cf5497e800fdbe217
Marcelinho <mh390150@gmail.com>
2026-09-11 11:11:27 -0300

    Adicionei as tabelas finais

--- banco de dados inicial.sql
+++ banco de dados inicial.sql
@@ -1,5 +1,5 @@
 create database mediaSaver;
-create table User(
+create table Usuar(
 id int primary key auto_increment,
 nome varchar(70) not null,
 email varchar(50) not null,
@@ -9,15 +9,46 @@ karma tinyint
 );
 create table Post(
 id int primary key auto_increment,
-idUser int,
+idUsuar int,
 titulo varchar(40) not null,
 descricao varchar(50),
 marca varchar(20),
 anoLancamento int unsigned,
 karma tinyint,
+qtdimagem tinyint unsigned default 0,
+qtdcomments tinyint unsigned default 0,
 primary key(id),
-foreign key (idUser) references User(id)
-)
-create table image(
+foreign key (idUsuar) references Usuar(id)
+);
+create table comentario(
+id smallint unsigned not null,
+conteudo text not null,
+idUsuar int not null,
+primary key(idUser, idPost, id),
+foreign key(idPost) references Post(id),
+foreign key (idUsuar) references Usuar(id)
+	);
+create table interacoes(
+idUsuar int unsigned not null,
+id int unsigned not null,
+idComentario smallint unsigned,
+idPost int unsigned,
+descricao varchar(10),
+data_hora datetime,
+primary key(idUsuar, id),
+foreign key (idUsuar) references Usuar(id),
+foreign key (idComentario) references Comentario(id),
+foreign key (idPost) references Post(id)
+);
+CREATE TABLE ImagemPost (
+    id INT,
+    idPost INT NOT NULL,
+    caminho VARCHAR(500) NOT NULL,
+	primary key(idPost,id),
+    
+    FOREIGN KEY (idPost)
+        REFERENCES Post(id)
+        ON DELETE CASCADE
+);
 
-	)
+    
